fc81dfa457641f10c73a6e0bcf63f3ed9f2750be
Joseph Lander <josephclander@gmail.com>
2022-12-29 16:07:01 +0000

    Add User Schema

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+app.use(express.json());
+const bodyParser = require("body-parser");
 const PORT = 3000;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // clean the req.body received
 app.use(
@@ -11,22 +12,48 @@ app.use(
   })
 );
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 // this sets the local mongo db name to 'auth_userDB'
-const mongoDB_URL = 'mongodb://localhost:27017/auth_userDB';
+const mongoDbURL =
+  process.env.MONGODB_URL || "mongodb://localhost:27017/auth_userDB";
 
 mongoose
-  .connect(mongoDB_URL, {
+  .connect(mongoDbURL, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .then(() => console.log(`MongoDB Connected at ${mongoDB_URL}`));
+  .then(() => console.log(`MongoDB Connected at ${mongoDbURL}`));
 
-app.get('/', (req, res) => {
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
   const { data } = req.body;
   res.send(data);
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      console.log();
+    } else {
+      res.send("success");
+    }
+  });
 });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+module.exports = app;
